85a4961fa6ae573ea545def0f5fb21061338d37c
김낙현 <sonata0226@gmail.com>
2020-10-21 16:53:07 +0900

    S03P31B102-89 / Add show unity

--- code/frontend/src/App.vue
+++ code/frontend/src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Ball/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Ball from './components/Ball.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Ball
   }
 }
 </script>
